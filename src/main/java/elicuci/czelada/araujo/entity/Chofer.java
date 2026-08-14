6759799e54681f0359d7101383e96d447e074f4a
Carloar09 <aroncenturion1@gmail.com>
2026-08-13 23:36:16 -0500

    tabla chofer acabada

--- src/main/java/elicuci/czelada/araujo/entity/Chofer.java
+++ src/main/java/elicuci/czelada/araujo/entity/Chofer.java
@@ -1,4 +1,30 @@
 package elicuci.czelada.araujo.entity;
 
+import elicuci.czelada.araujo.entity.enums.EstadoVehiculo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="choferes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Chofer {
+
+    private Long idChofer;
+
+    private String nombreCompleto;
+
+    private String dni;
+
+    private String licencia;
+
+    private String telefono;
+
+    private EstadoVehiculo estado;
+
+    private Vehiculo vehiculo;
 }
